8a2496c196c901afdff89d6567c0ebe3154a258c
henriksjstrand <hesj78@icloud.com>
2021-11-25 11:25:33 +0100

    komplett med swing och server

---
+++ src/Default/Client.java
@@ -0,0 +1,145 @@
+package Default;
+
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client(Socket socket, String username)  {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        } catch (IOException e){
+            closeAll(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    //The clientHandler will await an assigned ID, so we'll send that right away
+    public void sendAnswer(String correctAnswer){
+        //Replacement for future implementation of a GUI and buttons
+        Scanner uInput = new Scanner(System.in);
+
+        String packet = uInput.nextLine();
+        if(packet.equals(correctAnswer)){
+            sendPacket("correct");
+        }
+        else{
+            sendPacket("false");
+        }
+
+    }
+
+
+    //Lil method to not have to write all the below memes over and over again
+    public void sendPacket(String inputToSend){
+        try {
+            bufferedWriter.write(inputToSend);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e){
+            closeAll(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    //Header of 0 equals random bs
+    //Header of 1 equals question
+    //Header of 2 equals other client isn't done yet
+    //This whole method will be a blocking one, therefore we'll run it in a difference thread
+    public void listenForPacket(){
+        //new Thread(() -> {
+        String receivedPacket;
+        int header;
+
+        while(socket.isConnected()){
+            try{
+                receivedPacket = bufferedReader.readLine();
+
+                //Separating the header and the rest of the string
+                header = Integer.parseInt(String.valueOf(receivedPacket.charAt(0)));
+                receivedPacket = receivedPacket.substring(1);
+
+                switch (header){
+                    case 0 -> System.out.println(receivedPacket);
+                    case 1 -> questionHandling(receivedPacket);
+                    case 2 -> waitForOtherClient();
+                    default -> System.out.println("Problem with the header");
+                }
+            } catch (IOException e){
+                closeAll(socket, bufferedReader, bufferedWriter);
+            }
+        }
+
+        //}).start();
+    }
+
+
+    public void questionHandling(String questionPacket){
+        //Index 0 == tietl
+        //Index 1-4 == Svarsalternativ
+        //Index 5 == korrekta svaret
+        String[] splitQuestion = QuestionManaging.splitQuestion(questionPacket);
+
+        for (int i = 0; i < splitQuestion.length; i++) {
+            switch (i){
+                case 0 -> System.out.println(splitQuestion[i]);
+                case 1,2,3,4 -> System.out.println("Option " + i + ": " + splitQuestion[i]);
+                case 5 -> System.out.print("Please enter your answer: ");
+                default -> System.out.println("Pain i dont know whats happening");
+            }
+        }
+        sendAnswer(splitQuestion[5]);
+    }
+
+
+    public void closeAll(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try {
+            if (bufferedReader != null){
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null){
+                bufferedWriter.close();
+            }
+            if (socket != null){
+                socket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void waitForOtherClient() {
+        System.out.println("Awaiting other client to finish their question...");
+        listenForPacket();
+    }
+
+
+    public static void runLoop() throws IOException {
+        Scanner uInput = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String name = uInput.nextLine();
+
+        Socket socket = new Socket("localhost", 7777);
+        Client client = new Client(socket, name);
+        client.sendPacket(client.username);
+        client.listenForPacket();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        runLoop();
+
+
+    }
+}
---
+++ src/Default/Server.java
@@ -0,0 +1,49 @@
+package Default;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()){
+                //socket.accept() is a blocking method, this means that the program will stop here
+                //Until a connection is established
+                Socket socket = serverSocket.accept();
+                System.out.println("Successful connection");
+                //Objects of this class will each handle communication with one client
+                //The runnable interface is implemented on a class whose instances are executed by separate threads
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                //To handle multiple clients at once we need to assign every client a separate thread
+                //Otherwise our program is only able to communicate with a single client at once
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+
+                //Add it so that multiple games can play at once
+                ServerActions.startGame();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Questions.readQuestionsFromFile("Geografi");
+        Questions.readQuestionsFromFile("Historia");
+        Questions.readQuestionsFromFile("Kultur");
+
+        ServerSocket serverSocket = new ServerSocket(7777);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+}
